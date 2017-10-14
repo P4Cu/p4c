e5bcbde3cbfd443150cea1b2e63fdc1dad369d96
Andrzej Pacanowski <andrzej.pacanowski@gmail.com>
2017-10-14 17:09:29 +0200

    add install.sh

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "About to install p4c environment setup!"
+
+if [[ "$DIR" != "${HOME}/p4c" ]]; then
+	>&2 echo "have to be cloned to ${HOME}/p4c"
+	exit 1
+fi
+
+function install_file() {
+	local what=$1
+	local where=$2
+
+	if [[ -e "$where" ]]; then
+		echo "File $where exists. Remove?"
+		rm -rfi $where
+		if [[ -e "$where" ]]; then
+			echo "$what not installed"
+			return
+		fi
+	fi
+	echo "Installing $what to $where"
+	ln -si $what $where
+}
+
+install_file ${HOME}/p4c/etc/tmux.conf ${HOME}/.tmux.conf
+install_file ${HOME}/p4c/etc/bashrc ${HOME}/.bashrc
+install_file ${HOME}/p4c/etc/zshrc ${HOME}/.zshrc
+install_file ${HOME}/p4c/config/tmux ${HOME}/.tmux
+install_file ${HOME}/p4c/config/vim ${HOME}/.config/nvim
+install_file ${HOME}/p4c/config/alacritty ${HOME}/.config/alacritty
+install_file ${HOME}/.config/nvim ${HOME}/.vim
+
