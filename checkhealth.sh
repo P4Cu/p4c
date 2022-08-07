f9bf491e53729833eb1b1a74c842792db00dc5dd
Andrzej Pacanowski <andrzej.pacanowski@gmail.com>
2022-08-07 12:58:04 +0200

    theme gruvbox and truecolor

---
+++ checkhealth.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# TODO: check if running in TMUX (tests work better that way) env variable $TMUX
+
+# test if p4c/bin in path
+
+# nvim 7.2
+# tmux 2.2
+# zsh ??
+
+echo "About to run 'nvim :checkhealth'. Press <enter> to continue.. "; read
+nvim +:checkhealth
+
+echo "Testing truecolor support."
+test_truecolor.sh
+
+echo "About to check true color in nvim. Press <enter> to continue.. "; read
+nvim "+:term test_truecolor.sh"
+
