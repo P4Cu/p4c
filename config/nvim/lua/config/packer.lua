502539be11896864dbd075c90eb73cbfd6d7e35a
Andrzej Pacanowski <andrzej.pacanowski@gmail.com>
2023-11-07 20:31:40 +0100

    nvim: indent blankline updated

--- config/nvim/lua/config/packer.lua
+++ config/nvim/lua/config/packer.lua
@@ -207,10 +207,7 @@ return require('packer').startup({
     use {
       'lukas-reineke/indent-blankline.nvim',
       config = function()
-        require('indent_blankline').setup({
-          show_current_context = true,
-          space_char_blankline = " ",
-        })
+        require('ibl').setup({})
       end,
     }
 
