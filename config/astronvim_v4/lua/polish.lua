db6a4fdad5c0ec5c970315550ae6d0373ea68f30
Andrzej Pacanowski <andrzej.pacanowski@gmail.com>
2025-02-13 15:41:57 +0100

    nvim: highlight trailing whitespace

--- config/astronvim_v4/lua/polish.lua
+++ config/astronvim_v4/lua/polish.lua
@@ -2,6 +2,14 @@
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
+vim.fn.matchadd("EolSpaces", "/\\s\\+$/")
+vim.api.nvim_set_hl(0, "EolSpaces", { bg = "red" })
+
+vim.cmd [[ "
+highlight RedundantSpaces ctermbg=red guibg=red
+match RedundantSpaces /\s\+$/
+" ]]
+
 -- vim.cmd [[ source ~/.config/nvim/vimrc ]]
 
 -- -- -- Set up custom filetypes
